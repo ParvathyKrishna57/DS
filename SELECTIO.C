46ebf67350e38a1d147757c62b93e3f7b99eeb63
ParvathyKrishna57 <parvathykrishnakrishna91@gmail.com>
2026-01-05 19:21:37 +0530

    Add selection sort implementation with comments for clarity
    Updated the selection sort program with comments and improved formatting.

--- SELECTIO.C
+++ SELECTIO.C
@@ -1,34 +1,45 @@
 #include<stdio.h>
 #include<conio.h>
+
 void main()
 {
- int a[100],n,i,j,pos,swap;
- clrscr();
-    printf("enter the number of elements\n");
-    scanf("%d",&n);
-    printf("enter %d number\n",n);
-    for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
-    for(i=0;i<n-1;i++)
+    int a[100], n, i, j, pos, swap;
+    clrscr(); // Clear the screen (Turbo C specific)
+
+    // Input: number of elements
+    printf("Enter the number of elements\n");
+    scanf("%d", &n);
+
+    // Input: array elements
+    printf("Enter %d numbers\n", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    // Selection sort algorithm
+    for(i = 0; i < n - 1; i++)
     {
-      pos=i;
-      for(j=i+1;j<n;j++)
-      {
-	if(a[pos]>a[j])
-	pos=j;
-      }
-       if(pos!=i)
-       {
-	 swap=a[i];
-	 a[i]=a[pos];
-	 a[pos]=swap;
-       }
-      }
-       printf("sorted array is \n");
-       for(i=0;i<n;i++)
-       {
-	printf("%d\t",a[i]);
-       }
-       getch();
+        pos = i; // Assume current index is the minimum
+        for(j = i + 1; j < n; j++)
+        {
+            if(a[pos] > a[j]) // Find smaller element
+                pos = j;
+        }
+
+        // Swap if a smaller element was found
+        if(pos != i)
+        {
+            swap = a[i];
+            a[i] = a[pos];
+            a[pos] = swap;
+        }
     }
 
+    // Output: sorted array
+    printf("Sorted array is:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+
+    getch(); // Wait for key press before closing (Turbo C specific)
+}
